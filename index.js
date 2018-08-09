dfc01b01b62697ef7bad3ae8b3339ce8dc92fece
Andrew Zubiri <zubiri@gmail.com>
2018-08-09 23:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet() {
-  console.log('== scuberGreetingForFeet ==');
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    let response = 'This one is on me!'
+  }
 }
+
+scuberGreetingForFeet(199);
