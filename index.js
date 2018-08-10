2e5026fc4dbb30d307dc5a0ac9b76311bcca5f8b
Andrew Zubiri <zubiri@gmail.com>
2018-08-10 00:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return "This one is on me!";
   }
+
+  if (distance > 2000) {
+    return "I will gladly take your thirty bucks.";
+  
 }
 
 scuberGreetingForFeet(199);
