b103b5efbce544411e329662bbb0a6ab5d49b589
Andrew Zubiri <zubiri@gmail.com>
2018-08-10 00:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ scuberGreetingForFeet(2001);
 function ternaryCheckCity(destination) {
   console.log("== ternaryCheckCity ==");
   destination === "NYC"
-  ? 'Ok, sounds good.'
-  : 'No go.';
+  ? return 'Ok, sounds good.'
+  : return 'No go.';
 }
 
 ternaryCheckCity ("NYC")
